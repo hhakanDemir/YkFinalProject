65a79cd9e8958cd3b6e2b49d69c78a6b20665de6
Hakan Demir <hhkandemir@gmail.com>
2025-11-28 21:21:47 +0300

    ders arasına kadar geldik

--- Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae6eb8eb640c5875c2d67d2a062dc2fb670d6ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60290016494c8a062a67af5cf68c9717c2657aeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ConsoleUI/obj/Debug/net8.0/ConsoleUI.AssemblyInfo.cs
+++ ConsoleUI/obj/Debug/net8.0/ConsoleUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dae6eb8eb640c5875c2d67d2a062dc2fb670d6ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60290016494c8a062a67af5cf68c9717c2657aeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
